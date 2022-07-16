e2391ddafdd4f763c1f83f10c48ada742dc3a1ba
Maximilian Meffert <maxmeffert@intrawarez.de>
2022-07-17 00:27:41 +0200

    monorepo-uilder merge

--- source/monorepo-builder.php
+++ source/monorepo-builder.php
@@ -9,6 +9,10 @@ use Symplify\MonorepoBuilder\ValueObject\Option;
 return static function (ContainerConfigurator $containerConfigurator): void {
     $parameters = $containerConfigurator->parameters();
 
+    $parameters->set(Option::PACKAGE_DIRECTORIES,[
+        __DIR__ . '/packages'
+    ]);
+
     $parameters->set(Option::DATA_TO_APPEND, [
         ComposerJsonSection::REQUIRE_DEV => [
             'phpunit/phpunit' => '^9.5',
